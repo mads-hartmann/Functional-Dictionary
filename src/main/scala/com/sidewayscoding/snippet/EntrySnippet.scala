379e9b8f8bcb8f060e7e4333f9a46a2a14fd7e6c
Mads Hartmann <mads379@gmail.com>
2010-10-16 19:01:29 +0200

    Altered the 'add' page to work with the ram storage instead

--- src/main/scala/com/sidewayscoding/snippet/EntrySnippet.scala
+++ src/main/scala/com/sidewayscoding/snippet/EntrySnippet.scala
@@ -1,9 +1,9 @@
 package com.sidewayscoding.snippet
 
 import com.sidewayscoding.model._
-import xml.{Text, NodeSeq}
+import xml.{ Text, NodeSeq }
 import net.liftweb.util.Helpers._
-import net.liftweb.http.{S, RequestVar}
+import net.liftweb.http.{ S, RequestVar }
 import net.liftweb.http.S._
 import net.liftweb.http.SHtml._
 import net.liftweb.common._
@@ -13,18 +13,16 @@ class EntrySnippet {
   object name extends RequestVar[String]("")
   object description extends RequestVar[String]("")
 
-	def create(xhtml: NodeSeq): NodeSeq = {
-	  bind("entry", xhtml, 
-	    "name" -> text(name, str => name(str)),
-	    "description" -> text(description, str => description(str)),
-	    "submit" -> submit("Save entry", () => {
-	      val e = new Entry(name,description)
-	      Dictionary.add(e)
-	    })
-	  )
-	}
-		
-		
-		
+  def create(xhtml: NodeSeq): NodeSeq = {
+    bind("entry", xhtml,
+      "name" -> text(name, str => name(str)),
+      "description" -> textarea(description, str => description(str)),
+      "submit" -> submit("Save entry", () => {
+        val e = new Entry(name, description)
+        Dictionary.add(e)
+        S.notice("Horray! The dictionary just grew bigger")
+        redirectTo("/")
+      }))
+  }
 
 }
