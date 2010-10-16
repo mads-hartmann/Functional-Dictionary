81034451e506e69fa5a8f3149f49af6288a6aee9
Mads Hartmann <mads379@gmail.com>
2010-10-16 19:02:38 +0200

    The RAM storage now uses a SortedSet to store the Entries

--- src/main/scala/com/sidewayscoding/model/Dictionary.scala
+++ src/main/scala/com/sidewayscoding/model/Dictionary.scala
@@ -1,17 +1,20 @@
 package com.sidewayscoding.model
 
-import scala.collection.{SortedSet}
+import scala.collection.{ SortedSet }
 
-case class Entry(name: String, description: String) 
+case class Entry(name: String, description: String) extends Ordered[Entry] {
+  def compare(that: Entry) = name.compare(that.name)
+}
 
 object Dictionary {
-  
-  private var entries: List[Entry] = Nil
-  
-  def add(e: Entry) = entries = e :: entries
-  
-  def findAll = entries
-  
-  def findAllLike(str: String) = entries.filter( _.name.contains(str))
-  
+
+  private var entries = SortedSet[Entry]()
+
+  def add(e: Entry) = entries += e
+
+  def findAll = entries.toList
+
+  def findAllLike(str: String) =
+    entries.toList.filter(_.name.toLowerCase.contains(str.toLowerCase))
+
 }
