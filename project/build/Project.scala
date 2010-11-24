2dd779f8f219fd91ff66d9fc295fb38ac3940132
Mads Hartmann <mads379@gmail.com>
2010-11-24 23:43:27 +0100

    Bumped lift version to 2.8.1

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -18,8 +18,8 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) with stax.S
   val liftVersion = "2.2-M1"
 
   override def libraryDependencies = Set(
-    "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
-    "net.liftweb" %% "lift-testkit" % liftVersion % "compile->default",
+    "net.liftweb" % "lift-webkit_2.8.0" % liftVersion % "compile->default",
+    "net.liftweb" % "lift-testkit_2.8.0" % liftVersion % "compile->default",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
     "org.scala-tools.testing" % "specs" % "1.6.1" % "test->default"
