40ac21297cde715ad306686e401eb3764e890ea3
Mads Hartmann <mads379@gmail.com>
2010-12-08 16:55:55 +0100

    bumped lift version to 2.2-SNAPSHOT

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -15,11 +15,11 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) with stax.S
   val scalatoolsRelease = "Scala Tools Snapshot" at
   "http://scala-tools.org/repo-releases/"
 
-  val liftVersion = "2.2-M1"
+  val liftVersion = "2.2-SNAPSHOT"
 
   override def libraryDependencies = Set(
-    "net.liftweb" % "lift-webkit_2.8.0" % liftVersion % "compile->default",
-    "net.liftweb" % "lift-testkit_2.8.0" % liftVersion % "compile->default",
+    "net.liftweb" % "lift-webkit_2.8.1" % liftVersion % "compile->default",
+    "net.liftweb" % "lift-testkit_2.8.1" % liftVersion % "compile->default",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "junit" % "junit" % "4.5" % "test->default",
     "org.scala-tools.testing" % "specs" % "1.6.1" % "test->default"
