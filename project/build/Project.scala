4e82bc9bc35ab68303bf3fe5c592a9ea43e77734
Mads Hartmann <mads379@gmail.com>
2011-04-24 08:49:02 +0200

    Bumped lift version to 2.4-SNAPSHOT

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -1,17 +1,17 @@
 import sbt._
 
 class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) with stax.StaxPlugin {
-  
+
   val mavenLocal         = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
-  val scalatoolsSnapshot = "Scala Tools Snapshot"   at "http://scala-tools.org/repo-releases/"
-  val liftVersion        = "2.2"
+  val scalatoolsSnapshot = "Scala Tools Snapshot"   at "http://scala-tools.org/repo-snapshots/"
+  val liftVersion        = "2.4-SNAPSHOT"
 
   override def libraryDependencies = Set(
     "net.liftweb" % "lift-webkit_2.8.1" % liftVersion % "compile->default",
     "net.liftweb" % "lift-testkit_2.8.1" % liftVersion % "compile->default",
     "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
     "org.scala-tools.testing" % "specs" % "1.6.1" % "test->default",
-    // selenium 
+    // selenium
 	  "org.scalatest" % "scalatest" % "1.3" % "test->default",
 	  "org.seleniumhq.selenium" % "selenium" % "2.0b1" % "test->default",
 	  "org.seleniumhq.selenium" % "selenium-server" % "2.0b1" % "test->default"
